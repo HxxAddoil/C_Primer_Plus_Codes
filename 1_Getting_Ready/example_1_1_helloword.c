3104a3b4c070d88e5679363530b3c8cfa7e485ae
hxx <hxxaddoil@gmail.com>
2020-10-13 23:54:39 +0800

    add 1_Getting_Ready

---
+++ 1_Getting_Ready/example_1_1_helloword.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int fingers;
+
+	printf("How many fingers dou you have?\n");
+	scanf("%d", &fingers);
+	printf("So you have %d fingers, ha?", fingers);
+
+	return 0;
+}
---
+++ 1_Getting_Ready/example_1_2_concreate.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Concreate contains gravel and cements.\n");
+
+	return 0;
+}
