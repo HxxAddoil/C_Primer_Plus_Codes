090bcb7eef8220be4221e65029d82e38e7d5090a
hxx <hxxaddoil@gmail.com>
2020-10-15 00:59:05 +0800

    add 2_Introducing_C

---
+++ 2_Introducing_C/example_2_2_fathm_ft.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int feet, fatmoms;
+
+	fatmoms = 2;
+	feet = 6 * fatmoms;
+	printf("There are %d feet in %d fathmos!\n", feet, fatmoms);
+	printf("Yes I said %d feet!\n", fatmoms * 6);
+
+	return 0;
+	
+}
