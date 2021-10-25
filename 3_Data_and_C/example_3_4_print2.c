ecce23bf6caf2a2efd1eb33f7900020a755b72e9
hxx <hxxaddoil@gmail.com>
2021-10-25 23:34:17 +0800

    update 3.3~3.6

---
+++ 3_Data_and_C/example_3_4_print2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	unsigned int un = 3000000000;
+	short end = 200;
+	long big = 65537;
+	long long verybig = 12345678908642;
+
+	printf("un = %u and not %d\n", un, un);
+	printf("end = %hd and %d\n", end, end);
+	printf("big = %ld and not %hd\n", big, big);
+	printf("verybig = %lld and not %ld\n", verybig, verybig);
+
+	printf("sizeof end= %lu\n", sizeof end);
+	printf("sizeof big= %lu\n", sizeof big);
+	printf("sizeof verybig= %lu\n", sizeof verybig);
+
+	return 0;
+}
